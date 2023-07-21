8331b16844ac41e5b4d5d90c023df91943659146
«Marina <marinka377@gmail.com>
2023-07-21 14:56:48 +0300

    Все задачи решены

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+
+int Val (int N, int M)
+{
+    if (N == M) return M;
+    int temp = N + Val (N + 1 , M - 1) + M;
+    return temp;
+}
+
+
+Console.WriteLine();
+Console.WriteLine(Val (number1, number2));
---
+++ Zadacha68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите первое число число: ");
+uint m = uint.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число число: ");
+uint n = uint.Parse(Console.ReadLine());
+
+static uint Akkerman (uint m, uint n)
+{
+    if (m == 0) return n + 1;
+    else 
+    if ((m != 0) && (n == 0)) return Akkerman (m - 1, 1);
+    else
+    return Akkerman (m - 1, Akkerman (m, n - 1));
+}
+Akkerman (m, n);
+
+Console.WriteLine();
+Console.WriteLine (Akkerman(m,n));
